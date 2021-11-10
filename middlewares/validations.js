3b227f80b095a13ec685e484fff02559123cff61
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-11-10 16:52:12 -0300

    exercicio 3

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -2,7 +2,7 @@ const isValidEmail = (req, res, next) => {
   const { email } = req.body;
 
   if (!email || !email.length > 0) {
-    const msgObrigatorio = 'O campo \"email\" é obrigatório';
+    const msgObrigatorio = 'O campo "email" é obrigatório';
     return res.status(400).json({ message: msgObrigatorio });
   }
 
@@ -10,7 +10,7 @@ const isValidEmail = (req, res, next) => {
     !email.includes('@')
     || !email.includes('.com')
   ) {
-    const msgEmail = 'O \"email\" deve ter o formato \"email@email.com\"';
+    const msgEmail = 'O "email" deve ter o formato "email@email.com"';
     return res.status(400).json({ message: msgEmail });
   }
   next();
@@ -20,12 +20,12 @@ const isValidPassword = (req, res, next) => {
   const { password } = req.body;
 
   if (!password || !password.length > 0) {
-    const msgPassValid = 'O campo \"password\" é obrigatório';
+    const msgPassValid = 'O campo "password" é obrigatório';
     return res.status(400).json({ message: msgPassValid });
   }
 
   if (password.length < 6) {
-    const msgPass = 'O \"password\" deve ter pelo menos 6 caracteres';
+    const msgPass = 'O "password" deve ter pelo menos 6 caracteres';
     return res.status(400).json({ message: msgPass });
   }
   next();
