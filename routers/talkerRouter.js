9e2ada96298f442247e4ab0034656c2ed35a967f
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-11-10 11:37:27 -0300

    exercicio 1

---
+++ routers/talkerRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+  readContentFile,
+  writeContentFile,
+} = require('../helpers/readWriteFile');
+const PATH_FILE = './talker.json';
+
+router.get('/', async (_req, res) => {
+  const talker = await readContentFile(PATH_FILE) || [];
+
+  res.status(200).json({ talker });
+});
+
+module.exports = router;
