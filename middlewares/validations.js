7013917aabd0ebd51e8bf8a6aa75106b12d77ce0
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-11-10 21:45:59 -0300

    exercicio 3

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -31,7 +31,86 @@ const isValidPassword = (req, res, next) => {
   next();
 };
 
+const isValidToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const isTalkerNameValid = (req, res, next) => {
+  const { name } = req.body;
+  
+  if (!name || name.length <= 0) {
+    const msgNameError = 'O campo "name" é obrigatório';
+    return res.status(400).json({ message: msgNameError });
+  }
+  
+  if (name.length < 3) {
+    const msgNameInvalid = 'O "name" deve ter pelo menos 3 caracteres';
+    return res.status(400).json({ message: msgNameInvalid });
+  }
+
+  next();
+};
+
+const isTalkerAgeValid = (req, res, next) => {
+  const { age } = req.body;
+  
+  if (
+    !age
+    ) {
+      const msgAgeError = 'O campo "age" é obrigatório';
+      return res.status(400).json({ message: msgAgeError });
+  }
+
+  if (age < 18) {
+    const msgAgeInvalid = 'A pessoa palestrante deve ser maior de idade';
+    return res.status(400).json({ message: msgAgeInvalid });
+  }
+
+  next();
+};
+
+const isTalkerTalkValid = (req, res, next) => {
+  const { talk } = req.body;
+ 
+  if (!talk || !talk.watchedAt || !talk.rate) {
+        const msgField = 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
+        return res.status(400).json({ message: msgField });
+      }
+
+  const dateRegex = /(\d{2})\/(\d{2})\/(\d{4})/;
+  const dateIsValid = talk.watchedAt.match(dateRegex);
+  
+  if (!dateIsValid) {
+    const msgDateInvalid = 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+    return res.status(400).json({ message: msgDateInvalid });
+  }
+
+  next();
+};
+
+const isTalkerTalkRateValid = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (talk.rate < 1 || talk.rate > 5) {
+    const msgTalkRateErro = 'O campo "rate" deve ser um inteiro de 1 à 5';
+    return res.status(400).json({ message: msgTalkRateErro });
+  }
+  next();
+};
+
 module.exports = {
   isValidEmail,
   isValidPassword,
+  isValidToken,
+  isTalkerNameValid,
+  isTalkerAgeValid,
+  isTalkerTalkValid,
+  isTalkerTalkRateValid,
 };
--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const {
   readContentFile,
-  // writeContentFile,
+  writeContentFile,
 } = require('../helpers/readWriteFile');
+
+const {
+  isValidToken,
+  isTalkerNameValid,
+  isTalkerAgeValid,
+  isTalkerTalkValid,
+  isTalkerTalkRateValid,
+} = require('../middlewares/validations');
 
 const PATH_FILE = './talker.json';
 
@@ -14,12 +22,31 @@ router.get('/', async (_req, res) => {
 
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
-    const talkers = await readContentFile(PATH_FILE);
-    const talk = talkers.find((item) => item.id === parseInt(id, 10));
+  const talkers = await readContentFile(PATH_FILE);
+  const talk = talkers.find((item) => item.id === parseInt(id, 10));
 
-    if (!talk) return next({ statusCode: 404, message: 'Pessoa palestrante não encontrada' });
+  if (!talk) return next({ statusCode: 404, message: 'Pessoa palestrante não encontrada' });
 
-    res.status(200).json(talk);
+  res.status(200).json(talk);
 });
+
+router.post(
+  '/',
+  isValidToken,
+  isTalkerNameValid,
+  isTalkerAgeValid,
+  isTalkerTalkValid,
+  isTalkerTalkRateValid,
+  async (req, res) => {
+    const talkers = await readContentFile(PATH_FILE);
+    const newId = talkers.length + 1;
+    const newTalker = {
+      id: newId,
+      ...req.body,
+    };
+    await writeContentFile(PATH_FILE, newTalker);
+    res.status(201).json(newTalker);
+  },
+);
 
 module.exports = router;
